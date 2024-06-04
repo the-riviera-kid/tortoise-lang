58a53624093b16566bdf7f0f8906f0ef0c021059
GijsterHaar <97735298+GijsterHaar@users.noreply.github.com>
2024-06-04 15:57:08 +0200

    Tortoise: added option to choose from different files using sys.argv (#8)

--- tortoise.py
+++ tortoise.py
@@ -1,8 +1,17 @@
-def main():
-    FILENAME = 'tiny.tortoise'
+import sys
 
-    with open(FILENAME, 'r') as tiny_tortoise:
-        tortoise_program = tiny_tortoise.readlines()
+
+def main():
+    
+    try:
+        FILENAME = sys.argv[1]
+    except IndexError:
+        print('\nYou have to specify what tortoise you want executed.\
+              \nPlease add it as an argument on the command line.\n')
+        return
+
+    with open(FILENAME, 'r') as tortoise:
+        tortoise_program = tortoise.readlines()
 
     for i, line in enumerate(tortoise_program, 1):
         print(f'{i}. {line.strip()}')
