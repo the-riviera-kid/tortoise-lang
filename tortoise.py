4404ab38ff5c0ddd5a826127a19527ac0fdcbcd1
Carla Buongiorno <carla.buongiorno@fox-it.com>
2024-06-10 15:47:26 +0200

    Parse tortoise program commands

--- tortoise.py
+++ tortoise.py
@@ -4,7 +4,16 @@ def main():
     FILENAME = 'tiny.tortoise'
     with open(FILENAME, 'r') as tiny_tortoise:
         program = tiny_tortoise.readlines()
-    program = [i.strip() for i in program]
+    # Create a list of lists where each line of commands
+    # from the program are in a list. Strip the new lines
+    # to exclude them as elements in these lists. 
+    # If the command has two parts, the second element of the list
+    # becomes and empty string due to the space in the command.
+    # Remove the second element. 
+    program = [list(i.strip()) for i in program]
+    for i in program:
+        if len(i) > 1:
+            i.pop(1)
     tortoise_interpreter.main_tortoise(program)
 
 
