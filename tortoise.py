5717073cc64520cf47cce9dfb294981634ce72d5
Gijs ter Haar <gijs.terhaar@fox-it.com>
2024-06-17 15:42:37 +0200

    tortoise-testing: fetch file, check input, wrong input handling, get file content

--- tortoise.py
+++ tortoise.py
@@ -2,19 +2,36 @@ import sys
 
 
 def main():
-    
-    try:
-        FILENAME = sys.argv[1]
-    except IndexError:
-        print('\nYou have to specify what tortoise you want executed.\
-              \nPlease add it as an argument on the command line.\n')
-        return
+    VALID_ARGS = ['tiny.tortoise', 'medium.tortoise', 'large.tortoise']
 
+    FILENAME = get_tortoise_file(sys.argv, VALID_ARGS)
+    if not '.tortoise' in FILENAME:
+        print(FILENAME)
+        exit()
+
+    content = get_file_content(FILENAME)
+    for i in content:
+        print(i, end='\n')
+
+
+def get_tortoise_file(argv, VALID_ARGS):
+    try:
+        FILENAME = argv[1]
+    except IndexError:
+        return '\nPlease specify an argument on the command line\n'
+    if not argv[1] in VALID_ARGS:
+        return "\nThat's not an existing tortoise file\n"
+    return FILENAME
+
+
+def get_file_content(FILENAME):
     with open(FILENAME, 'r') as tortoise:
         tortoise_program = tortoise.readlines()
-
+    content = []
     for i, line in enumerate(tortoise_program, 1):
-        print(f'{i}. {line.strip()}')
+        content.append(f'{i}. {line.strip()}')
+    return content
+
 
 
 if __name__ == '__main__':
