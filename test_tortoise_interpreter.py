3c940e89048cb0e2b4f88ffa5aa3b305f78da30b
Carla Buongiorno <56362538+CarlaBuongiorno@users.noreply.github.com>
2024-07-30 15:37:20 +0200

    Static analysis (#19)
    * 13: Set up static analysis
    
    * Add tools.bash
    
    * Fix all formatting errors
    
    * Add tools.bash and fix formatting errors for test_tortoise_interpreter.py
    
    ---------
    
    Co-authored-by: Carla Buongiorno <carla.buongiorno@fox-it.com>

--- test_tortoise_interpreter.py
+++ test_tortoise_interpreter.py
@@ -1,46 +1,53 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 import pytest
 
 from tortoise_interpreter import (
     main_tortoise,
     sanitize_program,
     navigate_command_functions,
-    pen_up, pen_colour,
-    )
+    pen_up,
+    pen_colour,
+)
 
 
 def test_main_tortoise():
     program = [
-        'P 2  # select pen 2',
-        'D    # pen down',
-        'W 2  # draw west 2cm',
-        'N 1  # then north 1',
-        'E 2  # then east 2',
-        'S 1  # then back south',
-        'U    # pen up'
+        "P 2  # select pen 2",
+        "D    # pen down",
+        "W 2  # draw west 2cm",
+        "N 1  # then north 1",
+        "E 2  # then east 2",
+        "S 1  # then back south",
+        "U    # pen up",
     ]
     program = [list(i.strip()) for i in program]
 
     list_of_strings = []
+
     def print_function(stuff_to_print):
         list_of_strings.append(stuff_to_print)
 
     main_tortoise(program, print_function)
 
     assert list_of_strings == [
-        '1. P 2',
-        '2. PEN DOWN',
-        '3. Move 2 units to the west.',
-        '4. Move 1 unit to the north.',
-        '5. Move 2 units to the east.',
-        '6. Move 1 unit to the south.',
-        '7. PEN UP'
+        "1. P 2",
+        "2. PEN DOWN",
+        "3. Move 2 units to the west.",
+        "4. Move 1 unit to the north.",
+        "5. Move 2 units to the east.",
+        "6. Move 1 unit to the south.",
+        "7. PEN UP",
     ]
 
 
+# fmt: off
 @pytest.mark.parametrize('program,                         expected', [
-    (['P 3', 'U', 'D'],                                    [['P', '3'], ['U'], ['D']]),
-    (['P 3', 'U', 'D', 'P 3', 'U', 'D'],                   [['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D']]),
-    ([ 'P 3', 'U', 'D', 'P 3', 'U', 'D', 'P 3', 'U', 'D'], [['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D']]),
+    (['P 3', 'U', 'D'],                                   [['P', '3'], ['U'], ['D']]),
+    # pylint: disable=line-too-long
+    (['P 3', 'U', 'D', 'P 3', 'U', 'D'],                  [['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D']]),
+    (['P 3', 'U', 'D', 'P 3', 'U', 'D', 'P 3', 'U', 'D'], [['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D'], ['P', '3'], ['U'], ['D']]),
     ([
         'P 2  # select pen 2',
         'D    # pen down',
@@ -49,8 +56,10 @@ def test_main_tortoise():
         'E 2  # then east 2',
         'S 1  # then back south',
         'U    # pen up'
-    ],                                                     [['P', '2'], ['D'], ['W', '2'], ['N', '1'], ['E', '2'], ['S', '1'], ['U']]),
+    ],                                                    [['P', '2'], ['D'], ['W', '2'], ['N', '1'], ['E', '2'], ['S', '1'], ['U']]),
+    # pylint: enable=line-too-long
 ])
+# fmt: on
 def test_sanitized_programs(program, expected):
     program = [list(i.strip()) for i in program]
     sanitized_program = sanitize_program(program)
@@ -58,22 +67,22 @@ def test_sanitized_programs(program, expected):
 
 
 def test_navigate_command_functions():
-    def pen_up(command):
-        return f'PEN UP'
-    
-    def pen_colour(command):
-        return f'P {command[1]}'
+    def fake_pen_up(_):
+        return "PEN UP"
 
-    COMMANDS = {'U': pen_up, 'P': pen_colour}
-    program = [['P', '3'], ['U']]
-    command_to_parse = navigate_command_functions(COMMANDS, program)
+    def fake_pen_colour(command):
+        return f"P {command[1]}"
 
-    assert command_to_parse == ['1. P 3', '2. PEN UP']
+    commands = {"U": fake_pen_up, "P": fake_pen_colour}
+    program = [["P", "3"], ["U"]]
+    command_to_parse = navigate_command_functions(commands, program)
+
+    assert command_to_parse == ["1. P 3", "2. PEN UP"]
 
 
 def test_pen_up():
-    assert pen_up(['U']) == 'PEN UP'
+    assert pen_up(["U"]) == "PEN UP"
 
 
 def test_pen_colour():
-    assert pen_colour(['P', '2']) == 'P 2'
+    assert pen_colour(["P", "2"]) == "P 2"
--- tortoise_interpreter.py
+++ tortoise_interpreter.py
@@ -1,60 +1,91 @@
+"""Implementation of tortoise-language."""
+
+
 def main_tortoise(program, print_to_screen=print):
+    """Main function that interprets a tortoise program."""
     program = sanitize_program(program)
-    COMMANDS = {'U': pen_up, 'D': pen_down, 'P': pen_colour, 'N': move_north, 'S': move_south, 'E': move_east, 'W': move_west}
-    command_to_parse = navigate_command_functions(COMMANDS, program)
+    commands = {
+        "U": pen_up,
+        "D": pen_down,
+        "P": pen_colour,
+        "N": move_north,
+        "S": move_south,
+        "E": move_east,
+        "W": move_west,
+    }
+    command_to_parse = navigate_command_functions(commands, program)
 
     for command in command_to_parse:
         print_to_screen(command)
 
 
 def sanitize_program(program):
-    # If a command has two parts, the second element of the list will be an empty string.
-    # Remove this element. Then remove all elements after the first empty string in each list.
-    # These were the comments at the end of each command.
+    """Get tortoise program ready for interpretation.
+
+    There are unnecessary characters like spaces and comments which we want to get rid of.
+    If a command has two parts, the second element of the list will be an empty string.
+    Remove this element. Then remove all elements after the first empty string in each list.
+    These were the comments at the end of each command."""
     for command in program:
         if len(command) > 1:
             command.pop(1)
             if len(command) > 2:
-                del command[command.index(' '):]
+                del command[command.index(" ") :]
     return program
 
 
-def navigate_command_functions(COMMANDS, program):
-    # Comment
-    return [f'{i}. {COMMANDS[command[0]](command)}' for i, command in enumerate(program, 1)]
+def navigate_command_functions(commands, program):
+    """For each command, call the corresponding function.
+
+    Number each command of the tortoise program and call the
+    correct function for each command to be interpreted."""
+    return [
+        f"{i}. {commands[command[0]](command)}" for i, command in enumerate(program, 1)
+    ]
 
 
-def pen_up(command):
-    return f'PEN UP'
+# fmt: off
+def pen_up(_):
+# fmt: on
+    """Interprets the 'pen up' command."""
+    return "PEN UP"
 
 
-def pen_down(command):
-    return f'PEN DOWN'
+def pen_down(_):
+    """Interprets the 'pen down' command."""
+    return "PEN DOWN"
 
 
 def pen_colour(command):
-    return f'P {command[1]}'
+    """Selects a pen colour."""
+    return f"P {command[1]}"
 
 
 def move_north(command):
+    """Moves the pen north a certain distance in centimetres."""
     return format_direction_and_units(command, "north")
 
 
 def move_south(command):
+    """Moves the pen south a certain distance in centimetres."""
     return format_direction_and_units(command, "south")
 
 
 def move_east(command):
+    """Moves the pen east a certain distance in centimetres."""
     return format_direction_and_units(command, "east")
 
 
 def move_west(command):
+    """Moves the pen west a certain distance in centimetres."""
     return format_direction_and_units(command, "west")
 
 
 def format_string_for_singular_or_plural(command):
-    return f'units' if int(command[1]) > 1 else 'unit'
+    """Depending on the distance in the command, we use either 'unit' or 'units'."""
+    return 'units' if int(command[1]) > 1 else 'unit'
 
 
 def format_direction_and_units(command, direction):
+    """Returns the final, completed sentence."""
     return f"Move {command[1]} {format_string_for_singular_or_plural(command)} to the {direction}."
