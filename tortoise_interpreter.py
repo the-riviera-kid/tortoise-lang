b7941f6297f435c1feeb324bb7a04b0c3f0965f7
Gijs ter Haar <gijs.terhaar@fox-it.com>
2024-08-12 16:13:04 +0200

    ISSUE#12: Added Pen State Up or Down

--- tortoise_interpreter.py
+++ tortoise_interpreter.py
@@ -2,6 +2,7 @@ def main_tortoise(program, print_to_screen=print):
     program = sanitize_program(program)
     COMMANDS = {'U': pen_up, 'D': pen_down, 'P': pen_colour, 'N': move_north, 'S': move_south, 'E': move_east, 'W': move_west}
     command_to_parse = navigate_command_functions(COMMANDS, program)
+
     for command in command_to_parse:
         print_to_screen(command)
 
@@ -22,49 +23,44 @@ def navigate_command_functions(COMMANDS, program):
     command_to_parse = []
     pen_state = ''
     for i, command in enumerate(program, 1):
-        new_pen_state = get_pen_state(command, pen_state)
-        command_to_parse.append(f'{i}. {COMMANDS[command[0]](command, pen_state, new_pen_state)}')
-        pen_state = new_pen_state
+        result = COMMANDS[command[0]](command, pen_state)
+        if isinstance(result, tuple):
+            pen_state = result[1]
+            result = result[0]
+        command_to_parse.append(f'{i}. {result}')
     return command_to_parse
 
-
-def pen_up(command, pen_state, new_pen_state):
-    if new_pen_state != pen_state:
-        return f'PEN UP'
-    return f'PEN UP (ignored)'
-
-
-def pen_down(command, pen_state, new_pen_state):
-    if new_pen_state != pen_state:
-        return f'PEN DOWN'
-    return f'PEN DOWN (ignored)'
+def pen_up(command, pen_state):
+    if pen_state == "DOWN" or pen_state == '':
+        pen_state = "UP"
+        return f'PEN UP', pen_state
+    return f'PEN UP (ignored)', pen_state
 
 
-def get_pen_state(command, pen_state):
-        if command[0] == 'D':
-            return 'DOWN'
-        elif command[0] == 'U':
-            return 'UP'
-        return pen_state
+def pen_down(command, pen_state):
+    if pen_state == "UP" or pen_state == '':
+        pen_state = "DOWN"
+        return f'PEN DOWN', pen_state
+    return f'PEN DOWN (ignored)', pen_state
 
         
-def pen_colour(command, pen_state, new_pen_state):
+def pen_colour(command, pen_state):
     return f'P {command[1]}'
 
 
-def move_north(command, pen_state, new_pen_state):
+def move_north(command, pen_state):
     return format_direction_and_units(command, pen_state, "north")
 
 
-def move_south(command, pen_state, new_pen_state):
+def move_south(command, pen_state):
     return format_direction_and_units(command, pen_state, "south")
 
 
-def move_east(command, pen_state, new_pen_state):
+def move_east(command, pen_state):
     return format_direction_and_units(command, pen_state, "east")
 
 
-def move_west(command, pen_state,new_pen_state):
+def move_west(command, pen_state):
     return format_direction_and_units(command, pen_state, "west")
 
 
